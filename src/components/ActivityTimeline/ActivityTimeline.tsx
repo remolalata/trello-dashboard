452b57bc16c2c2c493593908c0de066428c10a72
Remo Antonio Lalata <lalata.remo@gmail.com>
2024-08-27 09:51:10 +0800

    Fix issue in dashboard

--- src/components/ActivityTimeline/ActivityTimeline.tsx
+++ src/components/ActivityTimeline/ActivityTimeline.tsx
@@ -16,18 +16,20 @@ const ActivityTimeline: React.FC = () => {
             {user && actions ?
                 actions.length > 0 &&
                     actions.map((action: any, index: number) => (
-                        <div className="flex items-start justify-between mb-6" key={index}>
-                            <div className="flex items-center gap-x-4">
-                                <div>
-                                    <Avatar alt="Remo Antonio Lalata" src={`${user.data.avatarUrl}/60.png`} />
+                        action?.data?.card?.name && (
+                            <div className="flex items-start justify-between mb-6" key={index}>
+                                <div className="flex items-center gap-x-4">
+                                    <div>
+                                        <Avatar alt="Remo Antonio Lalata" src={`${user.data.avatarUrl}/60.png`} />
+                                    </div>
+                                    <div>
+                                        <div className="font-bold text-sm mb-1 text-gray-700">{user.data.fullName}</div>
+                                        <div className="font-thin text-xs italic text-gray-600">made an update on <span className="font-medium">{action?.data?.card?.name || "Card"}</span></div>
+                                    </div>
                                 </div>
-                                <div>
-                                    <div className="font-bold text-sm mb-1 text-gray-700">{user.data.fullName}</div>
-                                    <div className="font-thin text-xs italic text-gray-600">made an update on <span className="font-medium">{action.data.card.name}</span></div>
-                                </div>
+                                <div className="font-light text-gray-400 text-xs italic">{parseTrelloDateToTimeAgo(action.date)}</div>
                             </div>
-                            <div className="font-light text-gray-400 text-xs italic">{parseTrelloDateToTimeAgo(action.date)}</div>
-                        </div>
+                        )
                     ))
                 : <Skeleton variant="rounded" height={200} className="w-full" />
             }
